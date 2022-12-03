f98699d1b181d96a49f5a00a39b01d0e41334d3a
PurityGwaro <puritygwaro@gmail.com>
2022-12-03 16:16:20 +0300

    prop drilling, delete feedback,conditioned classes and styling, styled components,state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react"
+import FeedbackList from "./components/FeedbackList"
 import Header from "./components/Header"
+import { FeedbackData } from "./data/FeedbackData"
 
 export default function App() {
+  const [feedback,setFeedback] = useState(FeedbackData)
+  const deleteFeedback = (id) =>{
+    // set the new array minus what is being deleted
+    if(window.confirm('are you sure you want to delete?')){
+      setFeedback(feedback.filter((item)=>item.id !== id))
+    }
+  }
   return (
     <>
-    <Header text='Hello World'/>
+    <Header/>
     <div className="container">
-      <h1>MY APP</h1>
+      <FeedbackList 
+      handleDelete={deleteFeedback}
+      feedback={feedback}/>
     </div>
     </>
   )
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,17 @@
+export const FeedbackData = [
+    {
+        id:1,
+        rating:10,
+        text:"lorem ipsum dolor sit amet consectetur adipisicing elit.lorem ipsum dolor sit amet consectetur adipisicing elit.lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        id:2,
+        rating:5,
+        text:"lorem ipsum dolor sit amet consectetur adipisicing elit.lorem ipsum dolor sit amet consectetur adipisicing elit.lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        id:3,
+        rating:8,
+        text:"lorem ipsum dolor sit amet consectetur adipisicing elit.lorem ipsum dolor sit amet consectetur adipisicing elit.lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+]
