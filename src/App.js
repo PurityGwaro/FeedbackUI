ecd71e625de7dff71da4a257306e522b7bc77a94
Purity Gwaro <puritygwaro@gmail.com>
2022-12-06 12:44:44 +0300

    about page and navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import {v4 as uuidv4} from 'uuid'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import FeedbackForm from "./components/FeedbackForm"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import Header from "./components/Header"
 import { FeedbackData } from "./data/FeedbackData"
+import AboutPage from "./pages/AboutPage"
+import AboutIconLink from "./components/AboutIconLink"
 
 
 export default function App() {
@@ -22,15 +25,29 @@ export default function App() {
   }
   
   return (
-    <>
+    <Router>
     <Header/>
     <div className="container">
+    <Routes>
+     <Route exact path="/"
+     element={
+      <>
       <FeedbackForm handleAdd={addFeedback}/>
       <FeedbackStats feedback={feedback}/>
       <FeedbackList 
       handleDelete={deleteFeedback}
       feedback={feedback}/>
+      </>
+     }
+     >
+     
+     </Route>
+      
+      <Route exact path="/about" element={<AboutPage/>}/>
+      </Routes>
+      
     </div>
-    </>
+    <AboutIconLink/>
+    </Router>
   )
 }
