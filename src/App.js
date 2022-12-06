48832bd2d506e4cf6adb6afe8eeadbed03c3d0fc
Purity Gwaro <puritygwaro@gmail.com>
2022-12-06 10:36:38 +0300

    adding new feedback to the display

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import {v4 as uuidv4} from 'uuid'
 import FeedbackForm from "./components/FeedbackForm"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
@@ -8,17 +9,23 @@ import { FeedbackData } from "./data/FeedbackData"
 
 export default function App() {
   const [feedback,setFeedback] = useState(FeedbackData)
+  const addFeedback = (newFeedback) =>{
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback,...feedback])
+    console.log(newFeedback);
+  }
   const deleteFeedback = (id) =>{
     // set the new array minus what is being deleted
     if(window.confirm('are you sure you want to delete?')){
       setFeedback(feedback.filter((item)=>item.id !== id))
     }
   }
+  
   return (
     <>
     <Header/>
     <div className="container">
-      <FeedbackForm/>
+      <FeedbackForm handleAdd={addFeedback}/>
       <FeedbackStats feedback={feedback}/>
       <FeedbackList 
       handleDelete={deleteFeedback}
